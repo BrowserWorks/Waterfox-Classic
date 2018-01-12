e2753f77bc056023922efa8c8f45bbe603fef1f6
Jon Coppeard <jcoppeard@mozilla.com>
2018-01-12 10:36:23 +0000

    Bug 1429783 - Reset frozen state in nsIScriptElement::LoseParserInsertedness() r=smaug

--- dom/script/nsIScriptElement.h
+++ dom/script/nsIScriptElement.h
@@ -157,7 +157,6 @@ public:
 
   void LoseParserInsertedness()
   {
-    mFrozen = false;
     mUri = nullptr;
     mCreatorParser = nullptr;
     mParserCreated = mozilla::dom::NOT_FROM_PARSER;
@@ -167,6 +166,13 @@ public:
       htmlScript->GetAsync(&async);
     }
     mForceAsync = !async;
+
+    // Reset state set by FreezeExecutionAttrs().
+    mFrozen = false;
+    mIsModule = false;
+    mExternal = false;
+    mAsync = false;
+    mDefer = false;
   }
 
   void SetCreatorParser(nsIParser* aParser)
