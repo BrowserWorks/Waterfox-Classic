0b21bd5d702e5b2dc3788bad245bbf5fea21b15c
Mike Hommey <mh+mozilla@glandium.org>
2018-07-05 14:58:09 +0900

    Bug 1469091 - Build the clang plugin as a host shared library. r=ted
    This adds just enough host shared library support for this one use case,
    but also takes shortcuts, because fully supporting host shared library
    is a deep rabbit hole I'm not ready to take just to fix --enable-lto
    --enable-clang-plugin on mac builds.
    
    One downside is that one my machine the plugin now takes > 80s to build,
    instead of 15s before, thanks to the lack of unified sources.

--- config/recurse.mk
+++ config/recurse.mk
@@ -172,8 +172,8 @@ widget/android/bindings/export: build/annotationProcessors/export
 xpcom/xpidl/export: xpcom/idl-parser/xpidl/export
 
 ifdef ENABLE_CLANG_PLUGIN
-$(filter-out config/host build/unix/stdc++compat/% build/clang-plugin/%,$(compile_targets)): build/clang-plugin/target build/clang-plugin/tests/target
-build/clang-plugin/tests/target: build/clang-plugin/target
+$(filter-out config/host build/unix/stdc++compat/% build/clang-plugin/%,$(compile_targets)): build/clang-plugin/host build/clang-plugin/tests/target
+build/clang-plugin/tests/target: build/clang-plugin/host
 endif
 
 # Interdependencies that moz.build world don't know about yet for compilation.
@@ -188,4 +188,4 @@ endif
 endif
 # Most things are built during compile (target/host), but some things happen during export
 # Those need to depend on config/export for system wrappers.
-$(addprefix build/unix/stdc++compat/,target host) build/clang-plugin/target: config/export
+$(addprefix build/unix/stdc++compat/,target host) build/clang-plugin/host: config/export
