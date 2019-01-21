9d33c88c88fdd85e4d9561d7e29fe2842d7cd51a
Jon Coppeard <jcoppeard@mozilla.com>
2019-01-21 14:26:24 +0000

    Bug 1518075 - Fix rooting hazard r=me on a CLOSED TREE

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -13096,7 +13096,7 @@ nsGlobalWindow::RunTimeoutHandler(Timeout* aTimeout,
         nsJSUtils::ExecutionContext exec(aes.cx(), global);
         rv = exec.Compile(options, handler->GetHandlerText());
 
-        JSScript* script = exec.MaybeGetScript();
+        JS::Rooted<JSScript*> script(aes.cx(), exec.MaybeGetScript());
         if (script) {
           LoadedScript* initiatingScript = handler->GetInitiatingScript();
           if (initiatingScript) {
