95206c1bd0dbaa6cc88f67efc03649793b17bda7
Olli Pettay <Olli.Pettay@helsinki.fi>
2021-10-12 19:44:41 +0000

    Bug 1730048, reorder the code in nsMenuFrame::PassMenuCommandEventToPopupManager a bit, r=mstange a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D127596

--- layout/xul/nsMenuFrame.cpp
+++ layout/xul/nsMenuFrame.cpp
@@ -1293,7 +1293,9 @@ nsMenuFrame::PassMenuCommandEventToPopupManager()
   nsXULPopupManager* pm = nsXULPopupManager::GetInstance();
   nsMenuParent* menuParent = GetMenuParent();
   if (pm && menuParent && mDelayedMenuCommandEvent) {
-    pm->ExecuteMenu(mContent, mDelayedMenuCommandEvent);
+    nsCOMPtr<nsIContent> content = mContent;
+    RefPtr<nsXULMenuCommandEvent> event = mDelayedMenuCommandEvent;
+    pm->ExecuteMenu(content, event);
   }
   mDelayedMenuCommandEvent = nullptr;
 }
