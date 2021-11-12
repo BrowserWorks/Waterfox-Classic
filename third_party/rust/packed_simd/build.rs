bf61a669088640d12a0cdcd39ebd665d8f5f90eb
hawkeye116477 <hawkeye116477@gmail.com>
2021-11-12 10:20:56 +0100

    Bug 1730397 - Update packed_simd to a version that supports rustc >= 1.56

--- third_party/rust/packed_simd/build.rs
+++ third_party/rust/packed_simd/build.rs
@@ -1,4 +1,4 @@
-use rustc_version::{version, Version};
+use rustc_version::{version_meta, Channel, Version};
 
 fn main() {
     println!("cargo:rustc-env=RUSTC_BOOTSTRAP=1");
@@ -7,8 +7,28 @@ fn main() {
     if target.contains("neon") {
         println!("cargo:rustc-cfg=libcore_neon");
     }
-    let ver = version().unwrap();
-    if ver >= Version::parse("1.54.0-alpha").unwrap() {
-        println!("cargo:rustc-cfg=use_const_generics");
+    let ver_meta = version_meta().unwrap();
+    let old_const_generics =
+        if ver_meta.semver < Version::parse("1.56.0-alpha").unwrap() {
+            true
+        } else if ver_meta.semver >= Version::parse("1.57.0-alpha").unwrap() {
+            false
+        } else {
+            match ver_meta.channel {
+                Channel::Stable | Channel::Beta => false,
+                Channel::Nightly | Channel::Dev
+                    if ver_meta
+                        .commit_date
+                        .as_deref()
+                        .map(|d| d < "2021-08-31")
+                        .unwrap_or(false) =>
+                {
+                    true
+                }
+                _ => false,
+            }
+        };
+    if old_const_generics {
+        println!("cargo:rustc-cfg=const_generics");
     }
 }
