Floorp-Projects/Floorp

Bug 1730434 - Limit length of allowCredentials set. r=dveditz, a=RyanVM
Differential Revision: https://phabricator.services.mozilla.com/D145228

dom/webauthn/WebAuthnManager.cpp
@@ -451,6 +451,8 @@ already_AddRefed<Promise> WebAuthnManager::MakeCredential(
   return promise.forget();
 }
 
+const size_t MAX_ALLOWED_CREDENTIALS = 20;
+
 already_AddRefed<Promise> WebAuthnManager::GetAssertion(
     const PublicKeyCredentialRequestOptions& aOptions,
     const Optional<OwningNonNull<AbortSignal>>& aSignal) {
@@ -521,6 +523,12 @@ already_AddRefed<Promise> WebAuthnManager::GetAssertion(
   CryptoBuffer rpIdHash;
   if (!rpIdHash.SetLength(SHA256_LENGTH, fallible)) {
     promise->MaybeReject(NS_ERROR_OUT_OF_MEMORY);
+    return promise.forget();
+  }
+
+  // Abort the request if the allowCredentials set is too large
+  if (aOptions.mAllowCredentials.Length() > MAX_ALLOWED_CREDENTIALS) {
+    promise->MaybeReject(NS_ERROR_DOM_SECURITY_ERR);
     return promise.forget();
   }
 

BrowserWorks/Waterfox-Classic

dom/webauthn/WebAuthnManager.cpp
@@ -520,6 +520,8 @@ WebAuthnManager::StartSign() {
   }
 }
 
+const size_t MAX_ALLOWED_CREDENTIALS = 20;
+
 already_AddRefed<Promise>
 WebAuthnManager::GetAssertion(nsPIDOMWindowInner* aParent,
                               const PublicKeyCredentialRequestOptions& aOptions)
@@ -573,6 +575,12 @@ WebAuthnManager::GetAssertion(nsPIDOMWindowInner* aParent,
   CryptoBuffer rpIdHash;
   if (!rpIdHash.SetLength(SHA256_LENGTH, fallible)) {
     promise->MaybeReject(NS_ERROR_OUT_OF_MEMORY);
+    return promise.forget();
+  }
+
+    // Abort the request if the allowCredentials set is too large
+  if (aOptions.mAllowList.Length() > MAX_ALLOWED_CREDENTIALS) {
+    promise->MaybeReject(NS_ERROR_DOM_SECURITY_ERR);
     return promise.forget();
   }
 